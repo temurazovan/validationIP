bba5db017115d5eec9f9c4b59bc5770b4fe049eb
Nadezhda <nadezhdadieslast@gmail.com>
2024-02-15 11:20:15 +0400

    start project

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct Net {
+    std::string firstSubnet;
+    std::string secondSubnet;
+    std::string thirdSubnet;
+    std::string fourthSubnet;
+};
+
+bool checkIP(std::string ip) {
+    for (char number : ip) {
+        if (number )
+    }
+}
+
+int main() {
+    std::string ip;
+    std::cout << "Enter IP: " << std::endl;
+    std::cin >> ip;
+
+}
