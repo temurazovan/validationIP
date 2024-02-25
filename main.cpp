041e9087e245474a37fadd9dae4eb2d0f1e2113f
Nadezhda <nadezhdadieslast@gmail.com>
2024-02-25 21:06:03 +0400

    added dot checking

--- main.cpp
+++ main.cpp
@@ -8,19 +8,19 @@ struct IpAddress {
 };
 
 bool checkDots(std::string ip) {
-    for (char symbol : ip) {
-        if (!isdigit(symbol) && symbol != '.'){
+    for (char symbol: ip) {
+        if (!isdigit(symbol) && symbol != '.') {
             return false;
         }
     }
-    for (int i = 0; i < ip.length(); i++){
-        int k = 0;
+    int k = 0;
+    for (int i = 0; i <= ip.length(); i++) {
         if (ip[0] == '.' || (ip[i] == '.' && i + 1 <= ip.length() && ip[i + 1] == '.')) {
             return false;
         }
         if (ip[i] == '.') {
             k++;
-            if (k != 4) {
+            if (k > 3) {
                 return false;
             }
         }
@@ -29,6 +29,32 @@ bool checkDots(std::string ip) {
     return true;
 }
 
+void parseIp(std::string ip) {
+    IpAddress result;
+    int dotCount = 0;
+
+    for (char symbol: ip) {
+
+        if (symbol == '.') {
+            dotCount++;
+            continue;
+        }
+        if (dotCount == 0) {
+            result.firstSubnet += symbol;
+        }
+        if (dotCount == 1) {
+            result.secondSubnet += symbol;
+        }
+        if (dotCount == 2) {
+            result.thirdSubnet += symbol;
+        }
+        if (dotCount == 3) {
+            result.fourthSubnet += symbol;
+        }
+
+    }
+
+}
 
 int main() {
     std::string ip;
